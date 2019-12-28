d11298f1d9d33d34639cb76f2e8828bef274799a
Gregory Ivo <gregorybivo@gmail.com>
2019-12-28 12:38:33 -0500

    Updates

---
+++ precache-manifest.951801b4d91689b881c843cc6a4a5d6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e50bda951b384426d9f2363caa2ef1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e25bc887203f9c52a40d",
+    "url": "/monsters-rolodex/static/css/main.ee2c99e3.chunk.css"
+  },
+  {
+    "revision": "53889d8fbe73248f251b",
+    "url": "/monsters-rolodex/static/js/2.810c0e21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.810c0e21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e25bc887203f9c52a40d",
+    "url": "/monsters-rolodex/static/js/main.6056df61.chunk.js"
+  },
+  {
+    "revision": "6a7c11db057edb0d6568",
+    "url": "/monsters-rolodex/static/js/runtime-main.882608a2.js"
+  }
+]);
